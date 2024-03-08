5b5766d32515a162227bc05e45a953296dbd9ff9
Avinay Basnet <basnetavinay@gmail.com>
2024-03-08 14:05:29 +0545

    Dashboard ui.

--- src/app/buxx/feature/buxx.component.ts
+++ src/app/buxx/feature/buxx.component.ts
@@ -1,19 +1,37 @@
 import { ChangeDetectorRef, Component, inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ToolbarComponent } from '../../shared/ui/toolbar/feature/toolbar.component';
-import { SaveTransaction, Transaction } from '../../shared/model/buxx.model';
+import { Operator, SaveTransaction, Transaction } from '../../shared/model/buxx.model';
 import { BuxxStore } from '../data-access/buxx.store';
-import { sub } from 'date-fns';
 import { MatCardModule } from '@angular/material/card';
 import { AsyncPipe, DatePipe, NgClass, NgForOf, NgIf } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
-import { supabase } from '../../../supabase/supabase';
 import { AuthStore } from '../../shared/data-access/auth/auth.store';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { API_DATA } from './transactions-mock';
 import { Observable } from 'rxjs';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn
+} from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+export const dateRangeValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const toDate = control.get('toDate');
+  const fromDate = control.get('fromDate');
+  return toDate && fromDate;
+};
 
 @Component({
   selector: 'as-buxx',
@@ -29,7 +47,14 @@ import { MatIconModule } from '@angular/material/icon';
     MatExpansionModule,
     AsyncPipe,
     DatePipe,
-    MatIconModule
+    MatIconModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatSelectModule
   ],
   templateUrl: './buxx.component.html',
   styleUrl: './buxx.component.scss',
@@ -38,26 +63,37 @@ import { MatIconModule } from '@angular/material/icon';
 export class BuxxComponent implements OnInit, OnDestroy {
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
-  transactionsObs!: Observable<Transaction[]>;
-  dataSource: MatTableDataSource<Transaction> = new MatTableDataSource<Transaction>(API_DATA);
-  readonly store = inject(BuxxStore);
+  private readonly store = inject(BuxxStore);
   readonly authStore = inject(AuthStore);
+  private readonly changeDetectorRef = inject(ChangeDetectorRef);
+  private readonly fb = inject(FormBuilder);
+
+  dataSource: MatTableDataSource<Transaction> = new MatTableDataSource<Transaction>(API_DATA);
+  transactionsObs!: Observable<Transaction[]>;
   transactions: Transaction[] = API_DATA;
-  changeDetectorRef = inject(ChangeDetectorRef);
+  queryForm!: FormGroup;
 
   ngOnInit(): void {
+    this.queryForm = this.fb.group({
+      toDate: this.fb.control<Date | null>(null),
+      fromDate: this.fb.control<Date | null>(null),
+      amount: this.fb.group({
+        operator: this.fb.control<Operator>('<'),
+        value: this.fb.control<number | null>(null)
+      }),
+      name: this.fb.control<string | null>(null)
+    }, { validators: [dateRangeValidator] });
     this.changeDetectorRef.detectChanges();
     this.dataSource.paginator = this.paginator;
     this.transactionsObs = this.dataSource.connect();
     // this.store.fetch$.next({
-      // amount: {
-      //   op: '<=',
-      //   value: 50000
-      // },
-      // fromDate: sub(new Date(), { days: 30 }),
-      // toDate: new Date()
+    // amount: {
+    //   op: '<=',
+    //   value: 50000
+    // },
+    // fromDate: sub(new Date(), { days: 30 }),
+    // toDate: new Date()
     // });
-
   }
 
   saveTransaction(transaction: SaveTransaction): void {
@@ -73,6 +109,10 @@ export class BuxxComponent implements OnInit, OnDestroy {
   }
 
   change(event: PageEvent) {
+
+  }
+
+  filter() {
 
   }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      scale: {
+        '150': '1.5'
+      },
+      screens: {
+        '2xl': {'max': '1535px'}, // => @media (max-width: 1535px) { ... }
+        'xl': {'max': '1279px'}, // => @media (max-width: 1279px) { ... }
+        'lg': {'max': '1023px'}, // => @media (max-width: 1023px) { ... }
+        'md': {'max': '767px'}, // => @media (max-width: 767px) { ... }
+        'sm': {'max': '639px'}, // => @media (max-width: 639px) { ... }
+      },
+      minWidth: {
+        '11/12': '91.666667%'
+      }
+    },
   },
   plugins: [],
 }
