119f88035124a6e0ed5cd06e59a0ae80cbb2ff0f
Avinay Basnet <basnetavinay@gmail.com>
2024-03-21 11:02:52 +0545

    Implement loading state on UI.

--- src/app/buxx/ui/summary/summary.component.ts
+++ src/app/buxx/ui/summary/summary.component.ts
@@ -2,11 +2,12 @@ import { Component, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { Summary } from '../../../shared/model/buxx.model';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'as-summary',
   standalone: true,
-  imports: [MatCardModule, MatIconModule],
+  imports: [MatCardModule, MatIconModule, NgClass],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss'
 })
